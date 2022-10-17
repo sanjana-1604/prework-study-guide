22001cf130449b184c0183cf4e6862acbc31aa42
Sanjana Sethiya <porwal.sanjana@yahoo.com>
2022-10-17 11:13:43 +0100

    Added JS file

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for(var i = 0; i< topics.length; i++)
+{
+
+    if (topics[i] === 'HTML') {
+    console.log("Let's study HTML!");
+    } else if (topics[i] === 'CSS') {
+    console.log("Let's study CSS!");
+    } else if (topics[i] === 'Git') {
+    console.log("Let's study Git!");
+    } else if (topics[i] === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+    } else {
+    console.log('Please try again!');
+    }
+}
+
