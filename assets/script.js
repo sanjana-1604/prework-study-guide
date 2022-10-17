ae262fadbb4cd5108fe841ec5b0886d73e209c49
Sanjana Sethiya <porwal.sanjana@yahoo.com>
2022-10-17 11:24:42 +0100

    Updated the js file

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,34 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for(var i = 0; i< topics.length; i++)
-{
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-    if (topics[i] === 'HTML') {
-    console.log("Let's study HTML!");
-    } else if (topics[i] === 'CSS') {
-    console.log("Let's study CSS!");
-    } else if (topics[i] === 'Git') {
-    console.log("Let's study Git!");
-    } else if (topics[i] === 'JavaScript') {
-    console.log("Let's study JavaScript!");
-    } else {
-    console.log('Please try again!');
+function selectTopic()
+{
+    for(var i = 0; i< topics.length; i++)
+    {
+
+        if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+        } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+        } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+        } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+        } else {
+        console.log('Please try again!');
+        }
     }
+
 }
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+    }
+  }
+  console.log('Here are the topics we learned through Prework:');
+  listTopics()
+  console.log('Which topic should we study first?');
+  selectTopic()
+
+
 
